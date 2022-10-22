b1347038ffb8874f67ad88ec1e24f3bc2a808cba
v-2maban <61125903+v-2maban@users.noreply.github.com>
2022-10-22 14:30:35 +1100

    Add files via upload

---
+++ GetObjectId.ps1
@@ -0,0 +1,16 @@
+﻿$Devices=Get-Content .\Computers.txt
+$OutputList = @()
+$Obj = @()
+
+
+foreach ($Device in $Devices) {
+    Write-Host "Searching for: $Device" -ForegroundColor Cyan
+    $Obj = Get-AzureADDevice -SearchString $Device | Where-Object {$_.DeviceTrustType -eq 'ServerAd'} | Select -ExpandProperty Objectid
+    $Outputlist += Get-AzureADDevice -ObjectId $Obj | Select DisplayName, ObjectId,DeviceTrustType
+    #$Objlist += $Obj
+}
+
+$OutputList | Out-File .\DevicewithObjectid.csv
+$OutputList | Select -ExpandProperty ObjectId | Out-File .\DevicewithObjectidonly.csv
+$OutputListAll = Get-AzureADDevice -All $true | Select DisplayName, ObjectId,  DeviceTrustType
+$OutputListAll | Out-File .\AllDeviceswithObjectid.csv

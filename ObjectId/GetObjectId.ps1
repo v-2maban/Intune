7c0d981e9b4f9cd862c876b4d145dcb9e77c18a8
v-2maban <61125903+v-2maban@users.noreply.github.com>
2022-10-22 15:08:10 +1100

    Update GetObjectId.ps1

--- ObjectId/GetObjectId.ps1
+++ ObjectId/GetObjectId.ps1
@@ -1,18 +1,19 @@
-Install-Module AzureAD
-Connect-AzureAD
+#Install-Module AzureAD
+#Connect-AzureAD
 $Devices=Get-Content .\Computers.txt
 $OutputList = @()
 $Obj = @()
-
+$Objtemplate="version:v1.0","Member object ID or user principal name [memberObjectIdOrUpn] Required"
 
 foreach ($Device in $Devices) {
     Write-Host "Searching for: $Device" -ForegroundColor Cyan
     $Obj = Get-AzureADDevice -SearchString $Device | Where-Object {$_.DeviceTrustType -eq 'ServerAd'} | Select -ExpandProperty Objectid
     $Outputlist += Get-AzureADDevice -ObjectId $Obj | Select DisplayName, ObjectId,DeviceTrustType
-    #$Objlist += $Obj
-}
+    $Objtemplate += Get-AzureADDevice -SearchString $Device | Where-Object {$_.DeviceTrustType -eq 'ServerAd'} | Select -ExpandProperty Objectid
+    }
 
 $OutputList | Out-File .\DevicewithObjectid.csv
 $OutputList | Select -ExpandProperty ObjectId | Out-File .\DevicewithObjectidonly.csv
 $OutputListAll = Get-AzureADDevice -All $true | Select DisplayName, ObjectId,  DeviceTrustType
 $OutputListAll | Out-File .\AllDeviceswithObjectid.csv
+$Objtemplate | Out-File .\CsvTemplateToUpload.csv

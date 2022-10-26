a7a49ff4643496dcf67406962aa5bdf1bb5a81a1
v-2maban <61125903+v-2maban@users.noreply.github.com>
2022-10-26 13:11:50 +1100

    Add files via upload

---
+++ Device Workload/GetWorkloads.ps1
@@ -0,0 +1,25 @@
+﻿$Output = @()
+$OutputList = @()
+#Get-IntuneManagedDevice -Filter "operatingsystem eq 'windows'" | get-msgraphallpages | select devicename
+$Devices = Get-IntuneManagedDevice -Filter "operatingsystem eq 'windows'" | get-msgraphallpages | Where-Object {$_.configurationManagerClientEnabledFeatures -like "*windowsUpdateForBusiness=True*"}  | select -expandproperty devicename
+
+
+Foreach ($Device in $Devices) {
+Write-Host "Getting workload info for: $Device" -ForegroundColor Green
+ $DeviceProp = Get-IntuneManagedDevice -Filter "devicename  eq '$Device'" | get-msgraphallpages | select configurationManagerClientEnabledFeatures
+
+ $Output      = New-Object PSObject -Property @{
+                    "DeviceName" = $Device
+                    Inventory = $DeviceProp.psobject.Properties.value.inventory
+                    modernApps = $DeviceProp.psobject.Properties.value.modernApps
+                    resourceAccess = $DeviceProp.psobject.Properties.value.resourceAccess
+                    deviceConfiguration = $DeviceProp.psobject.Properties.value.deviceConfiguration
+                    compliancePolicy = $DeviceProp.psobject.Properties.value.compliancePolicy
+                    windowsUpdateForBusiness = $DeviceProp.psobject.Properties.value.windowsUpdateForBusiness
+                    
+                    }
+                    $OutputList += $Output | Select DeviceName,windowsUpdateForBusiness,deviceConfiguration,resourceAccess,compliancePolicy,Inventory,modernApps
+                    
+}
+
+$OutputList | ft | Out-File .\DeviceWithWorkload.csv

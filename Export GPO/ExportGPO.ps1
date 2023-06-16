31dfa96ef149f0d5ea6da21a5b2f00ab23b43596
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2023-06-16 14:28:48 +1000

    Add files via upload

---
+++ Export GPO/ExportGPO.ps1
@@ -0,0 +1,9 @@
+﻿#$gponames = get-gpo -all | Where-Object {$_.DisplayName -like '*Desktop*'} | Select DisplayName,Id
+$gponames = get-gpo -all | Select DisplayName,Id
+ForEach ($gponame in $gponames)
+{
+$filename=$gponame.displayname
+$filepath="c:\gpo\export\"+"$filename"+".xml"
+Get-GPOReport -ReportType xml -Guid $gponame.Id -Path $filepath
+Write-Host "Saving Group POlicy: $filename" -ForegroundColor Green
+}

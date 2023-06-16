cfb58f4020dcebdb90b8af872370bf9ee4027f1e
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2023-06-16 14:44:34 +1000

    Add files via upload

---
+++ Export GPO/ExportGPO.ps1
@@ -0,0 +1,11 @@
+﻿#Saves the exported GPO (in xml format) under c:\GPOExport
+$GPONames = Get-GPO -All | Select-Object DisplayName,Id
+
+ForEach ($GPOName in $GPONames) {
+$Filename = $GPOName.DisplayName
+$Filepath="c:\GPOExport\"+"$Filename"+".xml"
+Get-GPOReport -ReportType Xml -Guid $GPOName.Id -Path $Filepath
+Write-Host "Saving Group POlicy: $filename" -ForegroundColor Green
+}
+
+

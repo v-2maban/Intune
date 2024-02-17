097ec0784bc21b152c07b144b7e79cc083fa4180
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2024-02-17 14:03:55 +1100

    Add files via upload

--- Custom Compliance/Check Chrome/Check-Chrome.ps1
+++ Custom Compliance/Check Chrome/Check-Chrome.ps1
@@ -11,7 +11,11 @@ $ApprovedChromeVersions="121.0.6167.141","121.0.6167.140"
 $ChromeVersionOnDevice=(Get-Item "C:\Program Files\Google\Chrome\Application\chrome.exe").VersionInfo.ProductVersion
 If($ApprovedChromeVersions.Contains($ChromeVersionOnDevice)) {
 $ApprovedChromeVersion=$true
-Write-Host "$ChromeVersionOnDevice is the approved Chrome version" -ForegroundColor Cyan
+Write-Host "$ChromeVersionOnDevice is the approved Chrome version" -ForegroundColor Green
+}
+
+Else {
+Write-Host "$ChromeVersionOnDevice is not the approved chrome version" -ForegroundColor Red
 }
 
 #Output from all parameters

18612d67ebc9c3fbcf2cf0d9c5f1c3dbe967f843
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2024-02-17 13:58:54 +1100

    Add files via upload

---
+++ Custom Compliance/Check Manufacturer/CheckListofManufacturer.ps1
@@ -0,0 +1,16 @@
+﻿$SupportedManufacturers="Lenovo","Microsoft Corporation","Dell Inc."
+$WMI_ComputerSystem = Get-WMIObject -class Win32_ComputerSystem
+$Manufacturer = $WMI_ComputerSystem.Manufacturer
+
+IF ($Manufacturer -in $SupportedManufacturers)
+{
+$ManufacturerSupport = @{Manufacturer = "Supported"}
+Write-Host "Manufacturer is supported" -ForegroundColor Green
+return $ManufacturerSupport | ConvertTo-Json -Compress
+
+}
+Else {
+$ManufacturerSupport = @{Manufacturer = "NotSupported"}
+Write-Host "Manufacturer is not supported" -ForegroundColor Red
+return $ManufacturerSupport | ConvertTo-Json -Compress
+}

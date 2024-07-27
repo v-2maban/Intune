bf8ec82400c8ee2e21a0460f2cf7d2253a15b9ba
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2024-07-27 12:44:11 +1000

    Add files via upload

---
+++ RenameComputerName/RenameVM/RenameComputerNameVM.ps1
@@ -0,0 +1,25 @@
+﻿$ManufacturerValue = (Get-WmiObject Win32_ComputerSystem).Manufacturer
+$ModelValue = (Get-WmiObject Win32_ComputerSystem).Model
+
+Write-Host "This is a Dell Device" -ForegroundColor Green
+$Serialnumber = (get-wmiobject -class win32_bios).serialnumber
+$SerialnumberNoHyphen = $serialnumber.Replace("-","")
+
+$LastCharSerialnumber = $SerialnumberNoHyphen.Remove(0,($SerialnumberNoHyphen.Length -7))
+$NewCompName = "AULT-D"+"$LastCharSerialNumber"
+
+$OGHostname=hostname
+
+
+If ($OGHostname -eq $NewCompName) {
+Write-Host "Correct hostname detected for $NewCompName" -ForegroundColor Cyan
+
+                                  }
+
+Else {
+Write-Host "Incorrect hostname detected, change required from $OGHostname to $NewCompName" -ForegroundColor Red
+
+#Rename-Computer -NewName $NewCompName -Force
+#shutdown.exe /g /t 600 /f /c "Restarting the computer"
+      }
+

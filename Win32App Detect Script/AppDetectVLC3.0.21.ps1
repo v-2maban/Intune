2d941ce7776b6660a1e350b7b18f2df41b58b35c
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2024-07-27 12:09:50 +1000

    Add files via upload

---
+++ Win32App Detect Script/AppDetectVLC3.0.21.ps1
@@ -0,0 +1,12 @@
+﻿$FilePath="C:\Program Files\VideoLAN\VLC\vlc.exe"
+$FileProperties=Get-Item $FilePath
+$FileName = $FileProperties.Name
+$FileVersion = $FileProperties.VersionInfo.Fileversion
+
+$FileNameExpected="vlc.exe"
+$FilePropertiesExpected="3.0.21"
+
+
+If($FileName -eq $FileNameExpected -and $FileVersion -eq $FilePropertiesExpected){
+write-host "Latest VLC Installed"
+}

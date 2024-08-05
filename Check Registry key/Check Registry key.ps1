a8f84d465e29f8a74c25b4a340fbb6155207d63f
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2024-08-05 21:02:06 +1000

    Add files via upload

---
+++ Check Registry key/Check Registry key.ps1
@@ -0,0 +1,24 @@
+﻿# Define the registry path and value name
+$RegistryPath = "HKLM:\Software\ANZ\Autopilot"
+$ValueName = "RenameComputerName"
+
+
+# Check if the registry key exists
+if (Test-Path $RegistryPath) {
+    Write-Output "Registry key exists: $RegistryPath"
+
+    
+    $RegistryValue = Get-ItemProperty -Path $RegistryPath -Name $ValueName -ErrorAction SilentlyContinue
+     
+    If($RegistryValue.$ValueName -eq "Success"){
+    Write-Output "Registry value '$ValueName' exists with value: $($RegistryValue.$ValueName)"
+    exit 0
+    }
+    else {
+        Write-Output "Registry value '$ValueName' does not exist."
+        exit 1
+    }
+} else {
+    Write-Output "Registry key does not exist: $RegistryPath"
+    exit 1
+}

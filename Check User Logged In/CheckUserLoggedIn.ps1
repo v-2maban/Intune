1d19646cf134a916f5303cf79f5b4fe698547aef
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2024-08-10 12:26:20 +1000

    Add files via upload

---
+++ Check User Logged In/CheckUserLoggedIn.ps1
@@ -0,0 +1,25 @@
+﻿<#$info = gwmi -class win32_computerSystem  -ea silentlycontinue | Select-Object username
+
+if ($info.username.Length -gt 0)
+
+{$Message = $info.username}
+else
+{ $Message = "No user is logged in locally"}
+
+$message#>
+
+# Get a list of user sessions
+$UserInFullOS=$false
+$Usersession = Get-CimInstance -ClassName Win32_ComputerSystem
+$UserName = $Usersession.UserName
+
+
+# Check if there are any user sessions
+if ($UserName -and $UserName -notmatch "defaultuser" ) {
+#if ($UserNames) {
+        Write-Output "UserLoggedIn"
+        
+} else {
+        Write-Output "NoUserLoggedIn"
+        
+}

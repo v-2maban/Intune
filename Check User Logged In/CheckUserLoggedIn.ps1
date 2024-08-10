1415340a73d133e62cc033401973e87f68883c55
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2024-08-10 13:12:34 +1000

    Update CheckUserLoggedIn.ps1

--- Check User Logged In/CheckUserLoggedIn.ps1
+++ Check User Logged In/CheckUserLoggedIn.ps1
@@ -1,13 +1,3 @@
-﻿<#$info = gwmi -class win32_computerSystem  -ea silentlycontinue | Select-Object username
-
-if ($info.username.Length -gt 0)
-
-{$Message = $info.username}
-else
-{ $Message = "No user is logged in locally"}
-
-$message#>
-
 # Get a list of user sessions
 $UserInFullOS=$false
 $Usersession = Get-CimInstance -ClassName Win32_ComputerSystem

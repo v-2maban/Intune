ca1e3a7aa35f90f1f6dbe01bd7dec7adc56b1da6
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2025-04-05 14:07:54 +1100

    Update Detect-AdobeDisableJavaScript.ps1

--- Remediations/Check-Chrome/Detect-AdobeDisableJavaScript.ps1
+++ Remediations/Check-Chrome/Detect-AdobeDisableJavaScript.ps1
@@ -1,4 +1,4 @@
-﻿Try {
+Try {
     $Registry = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown" -ErrorAction Stop).bDisableJavaScript
     If ($Registry -eq 1){
         Write-Host "Compliant"
@@ -6,11 +6,11 @@
     }
     else
     {
-    Write-Host "Not Compliant, initiating remediation script"
+    Write-Warning "Not Compliant, initiating remediation script"
     Exit 1
     }
 } 
 Catch {
-    Write-Host "Registry key does not exist, initiating remediation script"
+    Write-Warning "Registry key does not exist, initiating remediation script"
     Exit 1
 }

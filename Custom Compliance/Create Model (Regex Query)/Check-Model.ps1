f84d8ab60bce9d8237ba62286887d06a4b4488f4
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2025-04-06 11:27:32 +1000

    Add files via upload

---
+++ Custom Compliance/Create Model (Regex Query)/Check-Model.ps1
@@ -0,0 +1,39 @@
+﻿<#
+Supported models are:
+Dell Precision
+Surface devices
+Virtual Machine
+#>
+
+$WMI_ComputerSystem = Get-WMIObject -class Win32_ComputerSystem
+$Model = $WMI_ComputerSystem.Model
+$ModelSupported = $true
+
+Switch -Regex ($Model) {
+    #Dell Precision
+    "Precision" { 
+    Write-Output "$Model is supported"}
+    #Surface
+    "Surfce" { 
+    Write-Output "$Model is Supported"}
+    #Virtual Machine
+    "Virtal Machine" { 
+    Write-Output "$Model is Supported"
+         }
+   DEFAULT { 
+   $ModelSupported = $false
+} 
+        }
+
+
+If ($ModelSupported -eq $true) {
+$ModelSupport = @{ModelSupport = "Supported"}
+return $ModelSupport | ConvertTo-Json -Compress
+}
+
+Else {
+Write-Output "$Model is not the supported Model"
+
+}
+
+

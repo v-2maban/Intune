6a96ef1e533287b324090a3421075d67be304724
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2025-04-10 15:59:21 +1000

    Add files via upload

---
+++ Remediations/Folder Mahesh/CreateFolder-Remediate.ps1
@@ -0,0 +1,9 @@
+<#
+Version: 1.0
+Author: 
+- Manish Bangia (manishbangia.com)
+#> 
+
+Write-Output "Remediating missing folder"
+New-Item -Name "Mahesh" -Path "c:\" -Itemtype Directory
+Write-Output "Remediation completed for folder Mahesh"

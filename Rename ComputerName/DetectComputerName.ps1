7d0a28e2f38a2afb1657554c2694d6ce8bf3c19d
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2025-04-19 13:43:41 +1000

    Add files via upload

---
+++ Rename ComputerName/DetectComputerName.ps1
@@ -0,0 +1,39 @@
+﻿$ManufacturerValue = (Get-WmiObject Win32_ComputerSystem).Manufacturer
+$ModelValue = (Get-WmiObject Win32_ComputerSystem).Model
+$Serialnumber = (Get-WmiObject -Class win32_bios).serialnumber
+$SerialnumberNoHyphen = $Serialnumber.Replace("-","")
+$LastCharSerialnumber = $Serialnumber.Remove(0,($Serialnumber.Length -7))
+$OGHostname=$env:COMPUTERNAME
+
+
+switch -Regex ($ModelValue) {
+
+## For Microsoft Surface Models ##
+"Surface" {
+$FirstCharSerialnumber = $Serialnumber.Substring(0,9)
+$NewHostName = "AUSP"+"$LastCharSerialNumber"
+}
+
+## For Dell Precision and Dell Latitude Models ##
+"Precision|Latitude" {
+$NewHostName = "AULPDL"+"$Serialnumber"
+}
+
+## For Microsoft Virtual Machine ##
+"Virtual Machine" {
+$FirstCharSerialnumber = $SerialnumberNoHyphen.Substring(0,9)
+$NewHostName = "AUVM"+"$FirstCharSerialnumber"
+}
+
+                            }
+
+#Detect ComputerName
+If ($OGHostname -eq $NewHostName) {
+Write-Output "No need to rename $NewHostName"
+Exit 0
+                                  }
+
+Else {
+Write-Output "Incorrect hostname detected: $OGHostname"
+Exit 1
+     }
---
+++ Rename ComputerName/RenameComputerName.ps1
@@ -0,0 +1,43 @@
+﻿$ManufacturerValue = (Get-WmiObject Win32_ComputerSystem).Manufacturer
+$ModelValue = (Get-WmiObject Win32_ComputerSystem).Model
+$Serialnumber = (Get-WmiObject -Class win32_bios).serialnumber
+$SerialnumberNoHyphen = $Serialnumber.Replace("-","")
+$LastCharSerialnumber = $Serialnumber.Remove(0,($Serialnumber.Length -7))
+$OGHostname=$env:COMPUTERNAME
+
+
+switch -Regex ($ModelValue) {
+
+## For Microsoft Surface Models ##
+"Surface" {
+$FirstCharSerialnumber = $Serialnumber.Substring(0,9)
+Write-Output "This is a $ModelValue Device"
+$NewHostName = "AUSP"+"$LastCharSerialNumber"
+}
+
+## For Dell Precision and Dell Latitude Models ##
+"Precision|Latitude" {
+Write-Output "This is a $ModelValue Device"
+$NewHostName = "AULPDL"+"$Serialnumber"
+}
+
+## For Microsoft Virtual Machine ##
+"Virtual Machine" {
+Write-Output "This is a $ModelValue Device"
+$FirstCharSerialnumber = $SerialnumberNoHyphen.Substring(0,9)
+$NewHostName = "AUVM"+"$FirstCharSerialnumber"
+}
+
+                            }
+
+#Rename ComputerName
+If ($OGHostname -eq $NewHostName) {
+Write-Output "No need to rename $NewHostName"
+                                  }
+
+Else {
+Write-Output "Rename required from $OGHostname to $NewHostName"
+Rename-Computer -NewName $NewHostName -Force
+#shutdown.exe /g /t 600 /f /c "Restarting the computer"
+Start-Sleep -Seconds 30
+     }

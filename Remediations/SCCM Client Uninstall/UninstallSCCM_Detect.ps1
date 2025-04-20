19d1ad33573ffbc8724c7ac30632285fd30ef12e
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2025-04-20 14:14:29 +1000

    Add files via upload

---
+++ Remediations/SCCM Client Uninstall/UninstallSCCM_Detect.ps1
@@ -0,0 +1,23 @@
+# Detect SCCM Client Agent Script
+
+#Define SCCM Client Agent service Variable
+$SCCMClientAgent = "ccmexec"
+
+#Check for SCCM Client Agent service
+Try {
+If(Get-Service -Name $SCCMClientAgent -ErrorAction stop) {
+Write-Output "Non-Compliant: SCCM Client agent detected, remediation required"
+exit 1
+}
+
+else {
+Write-Output "Compliant: SCCM Client agent not detected"
+exit 0
+}
+}
+
+
+Catch {
+Write-Output "Compliant: SCCM Client agent not detected"
+exit 0
+}

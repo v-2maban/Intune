99fc400a56d04637313946574d41a7cc1b982025
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2025-04-21 16:47:19 +1000

    Delete Remediations/AdobeJavaUpdateDisable directory

--- Remediations/AdobeJavaUpdateDisable/Folder Mahesh/CreateFolder-Remediate.ps1
+++
@@ -1,9 +0,0 @@
-<#
-Version: 1.0
-Author: 
-- Manish Bangia (manishbangia.com)
-#> 
-
-Write-Output "Remediating missing folder"
-New-Item -Name "Mahesh" -Path "c:\" -Itemtype Directory
-Write-Output "Remediation completed for folder Mahesh"
--- Remediations/AdobeJavaUpdateDisable/Remediate-AdobeDisableJavaScript.ps1
+++
@@ -1,1 +0,0 @@
-﻿New-ItemProperty "HKLM:\SOFTWARE\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown" -Name "bDisableJavaScript" -Value 1 -PropertyType DWord -Force

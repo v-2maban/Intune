1e65675dfb399c91f5507748eaf5be737faeeb55
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2025-04-21 16:41:22 +1000

    Add files via upload

---
+++ Remediations/Create Folder if missing/CreateFolder_detect.ps1
@@ -0,0 +1,28 @@
+<#
+Version: 1.0
+Author: 
+- Manish Bangia (manishbangia.com)
+Script: CreateFolder_Detect.ps1
+Description: Create the folder if missing
+#> 
+
+$RemediationName="Rem: Create Folder"
+$FolderPath="C:\TestFolder"
+
+Try {
+    
+    If(Test-path $folderPath) {
+            Write-Host "Compliant: Folder Path Exists"
+            Exit 0
+            }
+
+    else    {
+            Write-Host "Non-Compliant: Path does not exist, Remediation Required for $RemediationName"
+            exit 1
+            }
+    }
+
+Catch {
+            Write-Host "Non-Compliant: Path does not exist, Remediation Required for $RemediationName"
+        Exit 1
+      }
---
+++ Remediations/Create Folder if missing/CreateFolder_remediate.ps1
@@ -0,0 +1,29 @@
+<#
+Version: 1.0
+Author: 
+- Manish Bangia (manishbangia.com)
+Script: CreateFolder_Remediate.ps1
+Description: Create the folder if missing
+#>
+
+$RemediationName="Rem: Create Folder"
+
+$RemediationName="Rem: Create Folder"
+$FolderPath="C:\TestFolder"
+
+Try {
+    
+    If(Test-path $folderPath) {
+            Write-Host "Compliant: Folder Path Exists"
+                              }
+
+    else    {
+        Write-Output "Remediating missing folder"
+        New-Item -Name "TestFolder" -Path "c:\" -Itemtype Directory | Out-Null
+            }
+    }
+
+Catch {
+            Write-Output "Remediation failed for $RemediationName"
+        Exit 1
+      }

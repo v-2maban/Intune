141d22644432fc15cf6957d322efa884d0c2732f
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2025-04-22 18:38:22 +1000

    Update CreateFolder_detect.ps1

--- Remediations/Create Folder if missing/CreateFolder_detect.ps1
+++ Remediations/Create Folder if missing/CreateFolder_detect.ps1
@@ -12,17 +12,17 @@ $FolderPath="C:\TestFolder"
 Try {
     
     If(Test-path $folderPath) {
-            Write-Host "Compliant: Folder Path Exists"
+            Write-Output "Compliant: Folder Path Exists"
             Exit 0
             }
 
     else    {
-            Write-Host "Non-Compliant: Path does not exist, Remediation Required for $RemediationName"
+            Write-Output "Non-Compliant: Path does not exist, Remediation Required for $RemediationName"
             exit 1
             }
     }
 
 Catch {
-            Write-Host "Non-Compliant: Path does not exist, Remediation Required for $RemediationName"
+            Write-Output "Non-Compliant: Path does not exist, Remediation Required for $RemediationName"
         Exit 1
       }

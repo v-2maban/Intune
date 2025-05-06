8fecc812a299823feda0b2faed88deb3ec11d86e
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2025-05-06 21:06:35 +1000

    Add files via upload

---
+++ Remediations/Rename and disable Local Admin/RenameDisableLocalAdmin_detect.ps1
@@ -0,0 +1,26 @@
+<#
+Version: 1.0
+Author: 
+- Manish Bangia (manishbangia.com)
+Script: RenameDisableLocalAdmin_detect.ps1
+Description: Rename and Disable Local Admin Account
+#> 
+
+$NewAdminUser = "LocalAdmin"
+
+$ExistingAdminUser = Get-LocalUser | where-object {$_.SID -like "S-1-5-*-500"}
+$ExistingAdminName = $ExistingAdminUser.Name
+$ExistingAdminEnabled = $ExistingAdminUser.Enabled
+
+
+#Check Local Administrator Account Name
+If($ExistingAdminName -ne $NewAdminUser) {
+Write-Output "Local Admin Account not as per the standards"
+Exit 1
+}
+
+#Check if Local Administrator Account is enabled
+If($ExistingAdminEnabled -eq $true) {
+Write-Output "Local Account is Enabled"
+Exit 1
+}
---
+++ Remediations/Rename and disable Local Admin/RenameDisableLocalAdmin_remediate.ps1
@@ -0,0 +1,26 @@
+<#
+Version: 1.0
+Author: 
+- Manish Bangia (manishbangia.com)
+Script: RenameDisableLocalAdmin_remediate.ps1
+Description: Rename and Disable Local Admin Account
+#>
+
+$NewAdminUser = "LocalAdmin"
+
+$ExistingAdminUser = Get-LocalUser | where-object {$_.SID -like "S-1-5-*-500"}
+$ExistingAdminName = $ExistingAdminUser.Name
+$ExistingAdminEnabled = $ExistingAdminUser.Enabled
+
+
+#Rename Local Administrator Account
+If($ExistingAdminName -ne $NewAdminUser) {
+Write-Output "Renaming Local Administrator account"
+Rename-LocalUser -Name $ExistingAdminName -NewName $NewAdminUser
+}
+
+#Disable Local Administrator Account
+If($ExistingAdminEnabled -eq $true) {
+Write-Output "Disabling Local Administrator account"
+Disable-LocalUser -Name $NewAdminUser
+}

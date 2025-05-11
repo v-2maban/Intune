b5391e000db718b97620365cc6fe71a67e0d8243
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2025-05-11 12:55:58 +1000

    Add files via upload

---
+++ Remediations/Restart Service/Detect_restartservice.ps1
@@ -0,0 +1,32 @@
+﻿<#
+Version: 1.0
+Author: 
+- Manish Bangia (manishbangia.com)
+Script: Detect_restartservice.ps1
+Description: Check if service is running
+#> 
+
+$RemediationName="*Rem: Check Service*"
+$ErrorActionPreference = "Stop"
+$CheckServiceName = "winmgmt"
+
+
+Try {
+$CheckServiceStatus = Get-Service -Name $CheckServiceName
+
+
+If ($CheckServiceStatus.Status -eq "Running") {
+    Write-Output "Remediation Compliant for $RemediationName"
+    Exit 0
+    }
+
+Else {
+    Write-Output "Remediation Non-Compliant for $RemediationName"
+    Exit 1
+    }
+}
+
+Catch {
+    Write-Output "Remediation will not run for $RemediationName as $CheckServiceName does not exists"
+    Exit 1
+    }

e9fba74963c5bd7fc3633b372dc3dcb60a39e0e8
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2025-05-19 20:14:51 +1000

    Add files via upload

---
+++ Custom Compliance/Check Model (Regex)/Check-Model.ps1
@@ -0,0 +1,47 @@
+﻿<#
+Supported models are:
+Dell Precision
+HP ZBook
+Surface devices
+Virtual Machine
+#>
+
+$CustomComplianceName="*CustomCompliance: Check Model*"
+
+
+$WMI_ComputerSystem = Get-WMIObject -class Win32_ComputerSystem
+$Model = $WMI_ComputerSystem.Model
+$ModelSupported = $false
+
+Switch -Regex ($Model) {
+    #Dell Precision
+    "Precision" { 
+    $ModelSupported = $true
+                }
+    #HP ZBook
+    "ZBook" { 
+    $ModelSupported = $true
+            }
+    #Surface
+    "Surface" { 
+    $ModelSupported = $true
+              }
+    #Virtual Machine
+    "Virtal Machine" { 
+    $ModelSupported = $true
+                     }
+   DEFAULT { 
+   $ModelSupported = $false
+} 
+        }
+
+
+If ($ModelSupported -eq $true) {
+$ModelSupport = @{ModelSupport = "Supported"}
+Write-Output "$CustomComplianceName $Model is the supported Model"
+return $ModelSupport | ConvertTo-Json -Compress
+}
+
+Else {
+Write-Output "$CustomComplianceName $Model is not the supported Model"
+     }

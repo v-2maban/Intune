0691bb077d65aa285189757633e8f9a8fd57faff
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2025-05-19 20:16:03 +1000

    Add files via upload

---
+++ Remediations/Restart Service/Detect_restartservice.ps1
@@ -0,0 +1,31 @@
+﻿<#
+Version: 1.0
+Author: 
+- Manish Bangia (manishbangia.com)
+Script: Detect_restartservice.ps1
+Description: Check if service is running
+#> 
+
+$RemediationName="*Rem: Check Service*"
+$ErrorActionPreference = "Stop"
+$CheckServiceName = "iphlpsvc"
+
+
+Try {
+$CheckServiceStatus = Get-Service -Name $CheckServiceName
+
+
+If ($CheckServiceStatus.Status -eq "Running") {
+    Write-Output "$RemediationName is Compliant for $CheckServiceName"
+    Exit 0
+    }
+
+Else {
+    Write-Output "$RemediationName is Non-Compliant for $CheckServiceName"
+    Exit 1
+    }
+}
+
+Catch {
+    Write-Output "Remediation will not run for $RemediationName as $CheckServiceName does not exists"
+      }
---
+++ Remediations/Restart Service/Remediate_restartservice.ps1
@@ -0,0 +1,27 @@
+﻿<#
+Version: 1.0
+Author: 
+- Manish Bangia (manishbangia.com)
+Script: Remediate_restartservice.ps1
+Description: Check if service is running
+#> 
+
+$RemediationName="*Rem: Check Service*"
+$ErrorActionPreference = "Stop"
+$CheckServiceName = "iphlpsvc"
+
+
+Try {
+$CheckServiceStatus = Get-Service -Name $CheckServiceName
+
+
+If ($CheckServiceStatus.Status -ne "Running") {
+    Write-Output "$RemediationName initiating..."
+    Start-Service -Name $CheckServiceName
+    }
+   }
+
+Catch {
+    Write-Output "$RemediationName failed as $CheckServiceName does not exists"
+    Exit 1
+    }

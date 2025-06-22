d6e938c46b010e3ab9aebea7e3bc86e6a59d2571
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2025-06-22 19:43:32 +1000

    Add files via upload

---
+++ ObjectId/GetObjectId v02.ps1
@@ -0,0 +1,28 @@
+﻿#Install-Module Entra
+
+If (get-installedmodule Microsoft.Entra) 
+{Write-Host "Entra Module already installed" -ForegroundColor Cyan }
+
+Else {
+Install-Module Microsoft.Entra
+}
+Connect-Entra
+Connect-MgGraph
+
+$Devices=Get-Content .\Computers.txt
+$OutputList = @()
+$Obj = @()
+$Objtemplate="version:v1.0","Member object ID or user principal name [memberObjectIdOrUpn] Required"
+
+foreach ($Device in $Devices) {
+    Write-Host "Searching for: $Device" -ForegroundColor Cyan
+    $Obj = Get-EntraDevice -SearchString $Device | Where-Object {$_.DeviceTrustType -eq 'AzureAd'} | Select -ExpandProperty ObjectId
+    $Outputlist += Get-EntraDevice -ObjectId $Obj | Select DisplayName, ObjectId,DeviceTrustType
+    $Objtemplate += Get-EntraDevice -SearchString $Device | Where-Object {$_.DeviceTrustType -eq 'AzureAd'} | Select -ExpandProperty ObjectId
+    }
+
+$OutputList | Out-File .\DevicewithDeviceId.csv
+$OutputList | Select -ExpandProperty ObjectId | Out-File .\DevicewithDeviceIdonly.csv
+#$OutputListAll = Get-AzureADDevice -All $true | Select DisplayName, ObjectId,  DeviceTrustType
+#$OutputListAll | Out-File .\AllDeviceswithObjectid.csv
+$Objtemplate | Out-File .\CsvTemplateToUpload.csv

71c82730d14c87208653b940b1cf38bb0bf1541f
Manish Bangia <61125903+v-2maban@users.noreply.github.com>
2025-12-16 21:30:03 +1100

    Update RenameComputerName.ps1

--- Rename ComputerName/RenameComputerName.ps1
+++ Rename ComputerName/RenameComputerName.ps1
@@ -1,4 +1,4 @@
-﻿$ManufacturerValue = (Get-WmiObject Win32_ComputerSystem).Manufacturer
+$ManufacturerValue = (Get-WmiObject Win32_ComputerSystem).Manufacturer
 $ModelValue = (Get-WmiObject Win32_ComputerSystem).Model
 $Serialnumber = (Get-WmiObject -Class win32_bios).serialnumber
 $SerialnumberNoHyphen = $Serialnumber.Replace("-","")
@@ -18,8 +18,15 @@ $NewHostName = "AUSP"+"$LastCharSerialNumber"
 ## For Dell Precision and Dell Latitude Models ##
 "Precision|Latitude" {
 Write-Output "This is a $ModelValue Device"
-$NewHostName = "AULPDL"+"$Serialnumber"
+$NewHostName = "AULTD"+"$Serialnumber"
 }
+
+## For HP ZBook Models ##
+"ZBook" {
+Write-Output "This is a $ModelValue Device"
+$NewHostName = "AULTH"+"$Serialnumber"
+}
+
 
 ## For Microsoft Virtual Machine ##
 "Virtual Machine" {
@@ -31,6 +38,19 @@ $NewHostName = "AUVM"+"$FirstCharSerialnumber"
                             }
 
 #Rename ComputerName
+If($NewHostName -ne $Null) {
+Write-Output "Model value is not Null"
+
+#Run the function RenameComputer
+RenameComputer
+}
+
+Else {
+Write-Output "No validation for ComputerName"
+}
+
+
+Function RenameComputer {
 If ($OGHostname -eq $NewHostName) {
 Write-Output "No need to rename $NewHostName"
                                   }
@@ -38,6 +58,7 @@ Write-Output "No need to rename $NewHostName"
 Else {
 Write-Output "Rename required from $OGHostname to $NewHostName"
 Rename-Computer -NewName $NewHostName -Force
-#shutdown.exe /g /t 600 /f /c "Restarting the computer"
-Start-Sleep -Seconds 30
+Start-Sleep -Seconds 60
      }
+
+}
